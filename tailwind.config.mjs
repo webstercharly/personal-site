3e65ac823ef6087dded8c52522d067b3dbb72b75
Charly Webster <charly.webster92@gmail.com>
2025-11-06 06:00:41 +0000

    fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        // Can extend colors here if needed
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
